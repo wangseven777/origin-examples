f56fd3dc47ac16dd3f5155f8301360f516b8b2c3
张中华 <956159241@qq.com>
2023-05-08 15:11:54 +0800

    添加示例中如何使用脚本和vue模板语法

--- Vue/Vue3项目框架/js/app.js
+++ Vue/Vue3项目框架/js/app.js
@@ -5,9 +5,9 @@ const updateContentByInnerHtml = async (menu) => {
   let content;
   if (menu.cache && _cachedView.has(menu.id)) {
     content = _cachedView.get(menu.id);
-    console.log('使用缓存内容......');
+    console.log("使用缓存内容......");
   } else {
-    console.log('请求接口......');
+    console.log("请求接口......");
     content = `这是测试显示内容，该内容可能来自接口请求, 参数： ${JSON.stringify(
       menu
     )}`;
@@ -16,25 +16,69 @@ const updateContentByInnerHtml = async (menu) => {
   dom.innerHTML = content;
 };
 
+const updateContentByVueComponent = async (menu) => {};
+
 const updateContentByDom = async (menu) => {
-    const dom = document.querySelector(".app-main");
-    let content;
-    if (menu.cache && _cachedView.has(menu.id)) {
-      content = _cachedView.get(menu.id);
-      console.log('使用缓存内容......');
-    } else {
-      console.log('请求接口......');
-      content =  `<h1>这是测试显示内容，该内容可能来自接口请求, 参数： ${JSON.stringify(
+  const dom = document.querySelector(".app-main");
+  let content;
+  if (menu.cache && _cachedView.has(menu.id)) {
+    content = _cachedView.get(menu.id);
+    console.log("使用缓存内容......");
+  } else {
+    console.log("请求接口......");
+    content = `
+      <div class="box">
+      <h1>这是测试显示内容，该内容可能来自接口请求, 参数： ${JSON.stringify(
         menu
-      )}</h1>`;
-      _cachedView.set(menu.id, content);
-    }
-    const fragDOM = document.createRange().createContextualFragment(content);
-    dom.innerHTML = '';
-    dom.appendChild(fragDOM);
+      )}</h1>
+      <div v-if="true">测试vue中的模板语法</div>
+      <div v-if="false">测试vue中的模板语法：false</div>
+</div>
+
+<script>
+// 解析vue语法
+(function() {
+  const {
+    createApp,
+    reactive,
+    toRefs,
+    ref,
+    defineComponent,
+    h,
+    computed,
+    onMounted,
+  } = Vue;
+  const vue3Composition = {
+    setup() {}
+  };
+  const app = createApp(vue3Composition);
+  for ([name, comp] of Object.entries(ElementPlusIconsVue)) {
+    app.component(name, comp);
+  }
+  app.use(ElementPlus).mount("#app-main .box");
+  return app;
+})();
+
+const dom = document.querySelector(".app-main");
+dom.style.backgroundColor = 'red';
+
+// 引入JQ并使用
+function callback() { $('#app-main').css({ 'color': 'white' }); }
+loadScript('https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.js', callback); // loadScript是全局公共方法，使用方式Fn(url, callback)
+
+// 使用完或者不需要使用时，目前只能迷惑性的在HTML中不显示，但实际已经装载，$依旧可以全局使用
+// 如果不想被使用，可以强制置空，$ = null;
+unloadScript('https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.js');
+</script>
+      `;
+    _cachedView.set(menu.id, content);
+  }
+  const fragDOM = document.createRange().createContextualFragment(content);
+  dom.innerHTML = "";
+  dom.appendChild(fragDOM);
 };
 
 // 接口请求
 // 自动追加
 // script脚本
-// vue组件， dom中使用vue语法
+// vue组件， dom中使用vue模板语法
--- Vue/Vue3项目框架/js/utils.js
+++ Vue/Vue3项目框架/js/utils.js
@@ -1,36 +1,57 @@
 const findTreeNode = (tree, id) => {
-    let stark = [];
-    stark = stark.concat(tree);
-    while (stark.length) {
-      const temp = stark.shift();
-      if (temp.children) {
-        stark = stark.concat(temp.children);
-      }
-      if (temp.id === id) {
-        return temp;
-      }
+  let stark = [];
+  stark = stark.concat(tree);
+  while (stark.length) {
+    const temp = stark.shift();
+    if (temp.children) {
+      stark = stark.concat(temp.children);
     }
-  };
+    if (temp.id === id) {
+      return temp;
+    }
+  }
+};
 
-  const searchTreeNodeParents = (map, id) => {
-    let t = [];
-    for (const element of map) {
-      const e = element;
-      if (e.id === id) {
-        //若查询到对应的节点，则直接返回
-        t.push(e);
+const searchTreeNodeParents = (map, id) => {
+  let t = [];
+  for (const element of map) {
+    const e = element;
+    if (e.id === id) {
+      //若查询到对应的节点，则直接返回
+      t.push(e);
+      break;
+    } else if (e.children && e.children.length !== 0) {
+      //判断是否还有子节点，若有对子节点进行循环调用
+      let p = searchTreeNodeParents(e.children, id);
+      //若p的长度不为0，则说明子节点在该节点的children内，记录此节点，并终止循环
+      if (p.length !== 0) {
+        p.unshift(e);
+        t = p;
         break;
-      } else if (e.children && e.children.length !== 0) {
-        //判断是否还有子节点，若有对子节点进行循环调用
-        let p = searchTreeNodeParents(e.children, id);
-        //若p的长度不为0，则说明子节点在该节点的children内，记录此节点，并终止循环
-        if (p.length !== 0) {
-          p.unshift(e);
-          t = p;
-          break;
-        }
       }
     }
-    return t;
-  };
+  }
+  return t;
+};
 
+// 动态添加JS脚本
+const loadScript = (src, onload) => {
+  const loaded = Array.from(document.scripts).some(it => it.getAttribute('src') === src); // Warn：script.src !== script.getAttribute('src')
+    if (loaded) {
+        typeof onload === 'function' && onload();
+        return;
+    }
+    const script = document.createElement('script');
+    script.src = src;
+    document.head.insertBefore(script, document.head.firstElementChild);
+    script.addEventListener('load', (ev) => {
+        typeof onload === 'function' && onload();
+    });
+};
+
+
+// 删除JS脚本
+const unloadScript = (url) => {
+    const script = document.querySelector(`script[src='${url}']`)
+    document.head.removeChild(script);
+};
