793eec9e600eedd734eb59f9e8d7d0f2c4ffac54
zhangzhonghua <956159241@qq.com>
2023-06-13 11:00:08 +0800

    优化结构

--- HTML/axios测试/request.js
+++
@@ -1,9 +0,0 @@
-async function post (url, params) {
-  const result = await axios.post(url,{params});
-  return result;
-};
-
-async function get (url, params) {
-  const result = await axios.get(url,{params});
-  return result;
-};
